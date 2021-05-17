9417dab0eb557a9be3df9626edc438e152d7a1fe
maxe23 <maxeiteljorg@gmail.com>
2021-05-16 22:35:19 -0400

    streetmap and layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -45,3 +45,12 @@ function mapColor(i) {
     }
 
     function createMap(earthquakes) {
+         // Define streetmap and darkmap layers
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  });
