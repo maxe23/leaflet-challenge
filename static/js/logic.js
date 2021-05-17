07152091b42f57cd69b6e83a50591bbdc4c190fa
maxe23 <maxeiteljorg@gmail.com>
2021-05-16 22:39:25 -0400

    legened

--- static/js/logic.js
+++ static/js/logic.js
@@ -97,3 +97,9 @@ function mapColor(i) {
     collapsed: false
   }).addTo(myMap);
 
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'info legend'),
+          depth = [-10, 10, 30, 50, 70, 90];
+
