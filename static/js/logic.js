ad2e2be6c21074e1a2a1e305a72df1a9c3bcfed1
maxe23 <maxeiteljorg@gmail.com>
2021-05-16 22:30:02 -0400

    request url

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,2 +1,11 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-05-05&endtime=2021-05-15";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+  
+    createFeatures(data.features);
+    console.log(data.features)
+  });
+  
