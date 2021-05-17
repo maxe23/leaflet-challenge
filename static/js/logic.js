a6d76dc0ecd8ce953c799002d73cd5601640a2c7
maxe23 <maxeiteljorg@gmail.com>
2021-05-16 22:36:37 -0400

    add darkmap

--- static/js/logic.js
+++ static/js/logic.js
@@ -54,3 +54,9 @@ function mapColor(i) {
     id: "mapbox/satellite-v9",
     accessToken: API_KEY
   });
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
